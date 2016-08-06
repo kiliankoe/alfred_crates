813dbfe3446561f8cf16842246e171fb847af957
Kilian Koeltzsch <me@kilian.io>
2016-08-06 10:51:13 +0200

    dummy script with basic idea, missing network

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+struct Crate {
+    name: String,
+    author: String,
+    link: String,
+}
+
+impl Crate {
+    fn to_xml(&self) -> String {
+        format!("<item arg=\"{}\">
+<title>{}</title>
+<subtitle>{}</subtitle>
+</item>", self.link, self.name, self.author)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let crate1 = Crate {
+        name: "crate1".to_owned(),
+        author: "author".to_owned(),
+        link: "https://github.com/author/crate1".to_owned(),
+    };
+    let crate2 = Crate {
+        name: "crate2".to_owned(),
+        author: "author".to_owned(),
+        link: "https://github.com/author/crate2".to_owned(),
+    };
+
+    let crates = [crate1, crate2];
+
+    println!("<?xml version=\"1.0\"?><items>");
+    for c in crates.iter() {
+        println!("{}", c.to_xml());
+    }
+    println!("</items></xml>");
 }
